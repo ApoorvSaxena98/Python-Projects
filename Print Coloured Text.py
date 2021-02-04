ed01980d346c8ad046586d584059889c4ace939d
Apoorv Saxena <47446077+ApoorvSaxena98@users.noreply.github.com>
2021-02-04 20:45:08 +0530

    Add files via upload

---
+++ Print Coloured Text.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Back, Style
+
+print(Fore.BLUE + Back.LIGHTYELLOW_EX + "Namaste! My Name Is Apoorv Saxena " + Fore.YELLOW+ Back.BLUE + "A Btech Graduate In CSE & A Job-Seeker")
+print(Fore.RED + Back.GREEN+ "Namaste! My Name Is Apoorv Saxena")
+
+print(Style.DIM + Fore.RED + Back.BLACK + 'some red text')
+print(Fore.LIGHTWHITE_EX + Back.LIGHTRED_EX + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print("Back to normal now...")
