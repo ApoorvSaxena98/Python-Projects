6ba4f6101f565320d0d134a20e5374c2d299659e
Apoorv Saxena <47446077+ApoorvSaxena98@users.noreply.github.com>
2021-02-04 14:42:51 +0530

    Update QR Code (2).py

--- QR Code (2).py
+++ QR Code (2).py
@@ -1,16 +1,11 @@
-#import pyqrcode
+import pyqrcode
 from pyqrcode import QRCode
 
 # String which represent the QR code
-#a = "https://www.google.co.in"
+a = "https://www.google.co.in"
 
 # Generate QR code
-#url = pyqrcode.create(a)
+url = pyqrcode.create(a)
 
 # Create and save the png file naming "pyqr.png"
-#url.svg("pyqr.svg", scale=6)
-
-
-import pyqrcode
-img = pyqrcode.create('https://www.google.co.in')
-img.svg('F:\PythonProjects\PP\pqr.svg', scale=4)
+url.svg("pyqr.svg", scale=6) # Here "pyqr.svg" is the file name which is being created in svg format.
