5c9385fe9a21c423926a96158168cb910562a0d3
Apoorv Saxena <47446077+ApoorvSaxena98@users.noreply.github.com>
2021-02-07 21:28:35 +0530

    Update Random Number Generation.py

--- Random Number Generation.py
+++ Random Number Generation.py
@@ -1,3 +1,6 @@
+# Both the codes will generate same o/p (1) is just for DICE ROLLING & (2) is for random number.
+
+# (1)
 import random
 min = 1
 max = 6
@@ -13,7 +16,7 @@ while roll_again == "yes" or roll_again == "y" or roll_again == "Y" or roll_agai
     roll_again = input("Roll the Dices Again? (Yes/No)")
 
 
-
+#(2)
 #import random
-#s = random.randint(0, 10)
+#s = random.randint(0, 10)  # 1st value is LOWER LIMIT & 2nd value is UPPER LIMIT 
 #print("Random number is: ", s)
